15fd5378b5c2c84ebffc495379657df19873f6bd
Tomi Poveda <tpovedatd@gmail.com>
2019-11-30 21:37:48 +0100

    Added Solstice Light Rig files

---
+++ solstice/files/lightrig.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains implementations for light rig files in Solstice
+"""
+
+from __future__ import print_function, division, absolute_import
+
+__author__ = "Tomas Poveda"
+__license__ = "MIT"
+__maintainer__ = "Tomas Poveda"
+__email__ = "tpovedatd@gmail.com"
+
+import logging
+
+import tpDccLib as tp
+
+from artellapipe.core import file
+
+LOGGER = logging.getLogger()
+
+
+class SolsticeLightRigFile(file.ArtellaFile, object):
+    def __init__(self, project, file_name, file_path=None, file_extension=None):
+        self._original_name = file_name.replace('_', ' ')
+        super(SolsticeLightRigFile, self).__init__(
+            project=project, file_name=file_name, file_path=file_path, file_extension=file_extension)
+
+    def get_template_dict(self):
+        """
+        Implements get_template_dict() function
+        :return: dict
+        """
+
+        return {
+            'project_path': self._project.get_path(),
+            'light_rig_name': self._original_name
+        }
+
+    def _reference_file(self, file_path, *args, **kwargs):
+        LOGGER.info('Referencing: {}'.format(file_path))
+        tp.Dcc.reference_file(file_path)
