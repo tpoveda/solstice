d7070bcd2c927474decf7675061a7aad59d082bd
Tomi Poveda <tpovedatd@gmail.com>
2020-03-08 14:37:44 +0100

    Added function to import Standin files in Solstice

--- solstice/files/shader.py
+++ solstice/files/shader.py
@@ -21,15 +21,15 @@ from Qt.QtCore import *
 from Qt.QtWidgets import *
 from Qt.QtGui import *
 
-import tpDccLib as tp
-from tpQtLib.core import qtutils
+import tpDcc as tp
+from tpDcc.libs.qt.core import qtutils
 
 import artellapipe
-from artellapipe.core import file, assetfile
+from artellapipe.core import defines, file, assetfile
 from artellapipe.utils import shader
 
 if tp.is_maya():
-    from tpMayaLib.core import shader as maya_shader
+    from tpDcc.dccs.maya.core import shader as maya_shader
 
 LOGGER = logging.getLogger()
 
@@ -46,10 +46,13 @@ class SolsticeShaderFile(file.ArtellaFile, object):
         :return: dict
         """
 
+        file_extension = kwargs.get('extension', None)
         return {
-            'project_path': self._project.get_path(),
-            'shaders_path': self._file_path,
-            'shader_name': self.name
+            'project_id': self._project.id,
+            'project_id_number': self._project.id_number,
+            'shaders': self._file_path,
+            'shader_name': self.name,
+            'file_extension': file_extension
         }
 
     def _import_file(self, file_path, *args, **kwargs):
@@ -92,59 +95,117 @@ class SolsticeShaderFile(file.ArtellaFile, object):
         return exported_shader
 
 
-class SolsticeShaderAssetFile(assetfile.ArtellaAssetFile, object):
+class SolsticeShadersAssetFile(assetfile.ArtellaAssetFile, object):
     def __init__(self, asset=None):
+        super(SolsticeShadersAssetFile, self).__init__(file_asset=asset)
+
+
+class SolsticeShaderAssetFile(assetfile.ArtellaAssetFile, object):
+    def __init__(self, asset=None, shader_name=None):
+        self._shader_name = shader_name
         super(SolsticeShaderAssetFile, self).__init__(file_asset=asset)
+
+    @property
+    def shader_name(self):
+        return self._shader_name
+
+    def get_template_dict(self, **kwargs):
+        """
+        Implements get_template_dict() function
+        :return: dict
+        """
+
+        template_dict = super(SolsticeShaderAssetFile, self).get_template_dict(**kwargs)
+        template_dict['shader_name'] = self._shader_name
+
+        return template_dict
+
+    def _import_file(self, file_path, *args, **kwargs):
+        LOGGER.info('Importing Asset Shader: {}'.format(file_path))
+        if not os.path.isfile(file_path):
+            LOGGER.warning('Shader File "{}" does not exists. Skip importing ...'.format(file_path))
+            return
+
+        if tp.Dcc.object_exists(self.name):
+            LOGGER.warning('Shader "{}" already exists in current scene. Skip importing ...'.format(self.name))
+            return
+
+        shader.ShadingNetwork.load_network(shader_file_path=file_path)
+
+    def _export_file(self, file_path, *args, **kwargs):
+
+        exported_shader = None
+        shader_swatch = kwargs.get('shader_swatch', None)
+
+        if not shader_swatch and tp.is_maya():
+            shader_swatch = maya_shader.get_shader_swatch(shader_name=self._shader_name)
+
+        px = QPixmap(QSize(100, 100))
+        shader_swatch.render(px)
+        temp_file = os.path.join(os.path.dirname(file_path), 'tmp.png')
+        px.save(temp_file)
+        try:
+            exported_shader = shader.ShadingNetwork.write_network(
+                shader_extension=self.extensions[0], shaders_path=os.path.dirname(file_path),
+                shaders=[self._shader_name], icon_path=temp_file)
+        except Exception as exc:
+            LOGGER.error('Error while exporting shader: {} | {}'.format(exc, traceback.format_exc()))
+        finally:
+            if temp_file and os.path.isfile(temp_file):
+                try:
+                    os.remove(temp_file)
+                except Exception:
+                    LOGGER.warning('Impossible to remove temporary swatch file: {}'.format(temp_file))
+
+        return exported_shader
 
 
 class SolsticeShaderMappingAssetFile(assetfile.ArtellaAssetFile, object):
     def __init__(self, asset=None, file_path=None):
         super(SolsticeShaderMappingAssetFile, self).__init__(file_asset=asset, file_path=file_path)
 
-        self._data = self._load_data()
+        self._data = dict()         # We do not initialize data here to avoid recursion calls
 
-    def get_shaders_info(self, force=False):
+    def get_shaders_info(self, force=False, status=defines.ArtellaFileStatus.PUBLISHED):
         """
         Returns dictionary that maps the shaders contained in the file with the different geometries
         :return: dict
         """
 
         if not self._data or force:
-            self._data = self._load_data()
+            self._data = self._load_data(status=status)
 
         return self._data
 
-    def get_shaders(self):
+    def get_shaders(self, status=defines.ArtellaFileStatus.PUBLISHED, force=False):
         """
         Returns a list contained in the shader file
         :return: list(str)
         """
 
         found_shaders = list()
-        for transform_name, shape_data in self.get_shaders_info().items():
-            for shape_name, shading_group_data in shape_data.items():
-                for shading_group_name, shaders_list in shading_group_data.items():
-                    for shader in shaders_list:
-                        if shader not in found_shaders:
-                            found_shaders.append(shader)
+        for transform_name, shape_data in self.get_shaders_info(status=status, force=force).items():
+            for shape_name, shaders_list in shape_data.items():
+                for shader in shaders_list:
+                    if shader not in found_shaders:
+                        found_shaders.append(shader)
 
         return found_shaders
 
-    def get_shading_geometry_mapping(self):
+    def get_shading_geometry_mapping(self, status=defines.ArtellaFileStatus.PUBLISHED):
         """
         Returns dictionary containing a relation with shapes and shaders to apply to
         :return:
         """
 
         shading_geo_mapping = dict()
-        for transform_name, shape_data in self.get_shaders_info().items():
-            for shape_name, shading_group_data in shape_data.items():
+        for shape_name, shape_data in self.get_shaders_info(status=status).items():
+            for shading_group, shaders_list in shape_data.items():
                 if shape_name not in shading_geo_mapping:
                     shading_geo_mapping[shape_name] = list()
-                for shading_group_name, shaders_list in shading_group_data.items():
-                    for shader in shaders_list:
-                        if shader not in shading_geo_mapping[shape_name]:
-                            shading_geo_mapping[shape_name].append(shader)
+                for shader in shaders_list:
+                    if shader not in shading_geo_mapping[shape_name]:
+                        shading_geo_mapping[shape_name].append(shader)
 
         return shading_geo_mapping
 
@@ -155,27 +216,27 @@ class SolsticeShaderMappingAssetFile(assetfile.ArtellaAssetFile, object):
         """
 
         shading_shader_mapping = dict()
-        for transform_name, shape_data in self._data.items():
-            for shape_name, shading_group_data in shape_data.items():
-                for shading_group_name, shaders_list in shading_group_data.items():
-                    if shading_group_name not in shading_shader_mapping:
-                        shading_shader_mapping[shading_group_name] = list()
-                    for shader in shaders_list:
-                        if shader not in shading_shader_mapping[shading_group_name]:
-                            shading_shader_mapping[shading_group_name].append(shader)
+        for shape_name, shape_data in self._data.items():
+            for shading_group_name, shaders_list in shape_data.items():
+                if shading_group_name not in shading_shader_mapping:
+                    shading_shader_mapping[shading_group_name] = list()
+                for shader in shaders_list:
+                    if shader not in shading_shader_mapping[shading_group_name]:
+                        shading_shader_mapping[shading_group_name].append(shader)
 
         return shading_shader_mapping
 
-    def _load_data(self):
+    def _load_data(self, status=defines.ArtellaFileStatus.PUBLISHED):
         """
         Internal function that loads the data contained in the file
         :return:
         """
 
-        if not self._file_path or not os.path.isfile(self._file_path):
+        file_path = self.get_file_paths(return_first=True, status=status)
+        if not file_path or not os.path.isfile(file_path):
             return dict()
 
-        with open(self._file_path, 'r') as f:
+        with open(file_path, 'r') as f:
             return json.load(f)
 
     def _export_file(self, file_path, *args, **kwargs):
--- solstice/files/standin.py
+++ solstice/files/standin.py
@@ -12,9 +12,33 @@ __license__ = "MIT"
 __maintainer__ = "Tomas Poveda"
 __email__ = "tpovedatd@gmail.com"
 
+import logging
+
+import tpDcc as tp
+
+import artellapipe
 from artellapipe.core import assetfile
+
+LOGGER = logging.getLogger()
 
 
 class SolsticeStandinAssetFile(assetfile.ArtellaAssetFile, object):
     def __init__(self, asset=None):
         super(SolsticeStandinAssetFile, self).__init__(file_asset=asset)
+
+    def _import_file(self, file_path, *args, **kwargs):
+        namespace = kwargs.get('namespace', None)
+        unique_namespace = kwargs.get('unique_namespace', True)
+        if not namespace:
+            namespace = self.asset.get_id()
+            if tp.Dcc.namespace_exists(namespace) and unique_namespace:
+                namespace = tp.Dcc.unique_namespace(namespace)
+
+        if tp.Dcc.namespace_exists(namespace) and unique_namespace:
+            namespace = tp.Dcc.unique_namespace(namespace)
+
+        ass_node = artellapipe.Arnold().import_standin(
+            file_path, namespace=namespace, unique_namespace=unique_namespace)
+        ass_node_parent = tp.Dcc.node_parent(node=ass_node)
+
+        return ass_node_parent
